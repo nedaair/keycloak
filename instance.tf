8412ba6f9197c997007c45429fa133b54336e8ba
nedapro <nedapro@mz.co.kr>
2021-06-10 12:58:31 +0900

    associate security group

--- instance.tf
+++ instance.tf
@@ -32,9 +32,8 @@ resource "aws_instance" "keycloak_instance" {
     user_data = <<-EOF
 		#! /bin/bash
         #install java
-        sudo apt-add-repository ppa:webupd8team/java
         sudo apt-get update
-        sudo apt-get install oracle-java8-installer
+        sudo apt-get install openjdk-8-jdk
 
         #install keycloak
         sudo wget https://github.com/keycloak/keycloak/releases/download/13.0.1/keycloak-13.0.1.tar.gz
